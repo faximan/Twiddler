39e115fa9f028897b28dc9e0430a053f6ee49ffd
Alexander Faxå <alexander.faxa@gmail.com>
2014-01-23 14:16:32 +0100

    Merged welcome.html with client.html

--- client.js
+++ client.js
@@ -2,6 +2,11 @@ displayView = function(){
     // the code required to display a view
 };
 
-document.onload = function(){
-    //code that is executed as the page is loaded.
+window.onload = function(){
+    main_div = document.getElementById('mainContent');
+    welcome_view = document.getElementById('welcomeview');
+
+    // welcome_view is a <script> element, take all inner
+    // HTML content and insert into main div.
+    main_div.innerHTML = welcome_view.innerHTML;
 };
