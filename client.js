8a8ad24c3e8c44ba536c5274e29e5bb3d5d76733
Alexander Faxå <alexander.faxa@gmail.com>
2014-01-25 16:36:27 +0100

    Finished client side (lab 1).

--- client.js
+++ client.js
@@ -1,19 +1,27 @@
-displayView = function(){
-  // the code required to display a view
-};
 
-// Display the desired view HTML based on whether the user is logged
-// in or not.
-var setCurrentView = function(){
+// The user the logged in user has searched for. This is used for state
+// keeping to ensure that the newest messages are loaded every time the
+// "Browse" view is opened and so that the HTML for showing user info
+// and the wall of messages can be shared between the Home and the Browse
+// tabs.
+var browsedUserActive = "";
+
+// The code required to display a view.
+displayView = function(){
   var main_div = document.getElementById('mainContent');
-  if (localStorage.token != "" && localStorage.token != undefined)
+
+  // Check if the user is logged in.
+  if (localStorage.token != "" && localStorage.token != undefined) {
+    // Load logged in user data.
     main_div.innerHTML = document.getElementById('profileview').innerHTML;
-  else
+    setupProfileView();
+  } else {
     main_div.innerHTML = document.getElementById('welcomeview').innerHTML;
+  }
 };
 
 window.onload = function(){
-  setCurrentView();  // Decides whether to show welcome or profile view.
+  displayView();  // Decides whether to show welcome or profile view.
 };
 
 // Add red error border to element.
@@ -44,7 +52,12 @@ var clearChangePwdStatusMsg = function() {
   document.getElementById("changePwdStatus").innerText = "";
 }
 
-// Add class "error" to all input fields that are empty in given form.
+// Clears the status message of the search user form.
+var clearSearchUserStatusMsg = function() {
+  document.getElementById("searchUserStatus").innerText = "";
+}
+
+// Add class "error" to all input fields that are empty in passed in form.
 var validateNonEmpty = function(form){
   var all_ok = true;
   var inputFields = form.getElementsByTagName("input");
@@ -52,6 +65,13 @@ var validateNonEmpty = function(form){
     if ( (inputFields[i].type == "text" ||
           inputFields[i].type == "password") &&
          inputFields[i].value == "") {
+      addError(inputFields[i]);
+      all_ok = false;
+    }
+  }
+  inputFields = form.getElementsByTagName("textarea");
+  for (var i = 0; i < inputFields.length; i++) {
+    if (inputFields[i].value == "") {
       addError(inputFields[i]);
       all_ok = false;
     }
@@ -75,7 +95,7 @@ var validateLoginForm = function(){
     } else {
       var token = result["data"];
       localStorage.token = token;  // Store token in HTML5 local storage.
-      setCurrentView();
+      displayView();
     }
   }
 };
@@ -108,10 +128,12 @@ var validateSignupForm = function(){
     var gender = form["gender"];
     var city = form["city"];
     var country = form["country"];
+    var password1 = form["password1"];
+    var password2 = form["password2"];
 
     var result = serverstub.signUp({
       "email": email.value,
-      "password": pwd1.value,
+      "password": password1.value,
       "firstname": firstname.value,
       "familyname": familyname.value,
       "gender": gender.value,
@@ -123,8 +145,8 @@ var validateSignupForm = function(){
       addError(email);
     } else {  // Signup succeeded. Clear all boxes.
       email.value = "";
-      pwd1.value = "";
-      pwd2.value = "";
+      password1.value = "";
+      password2.value = "";
       firstname.value = "";
       familyname.value = "";
       gender.value = "";
@@ -136,12 +158,12 @@ var validateSignupForm = function(){
 };
 
 // Selects and displays the tab with the given name.
-var selectTab = function(tabName, tabSelectorName) {
+var selectTab = function(tabName, tabSelector) {
   // Change which tab to be visible.
   var tabs = document.getElementById('tabs');
   for (var i = 0; i < tabs.childNodes.length; i++) {
     var child = tabs.childNodes[i];
-    if (child.className === "tab") {
+    if (child.className == "tab") {
       // Show only current tab.
       if (child.id == tabName) {
 	      child.style.display = "block";
@@ -155,16 +177,112 @@ var selectTab = function(tabName, tabSelectorName) {
   var tabSels = document.getElementById('tabSelectors');
   for (i = 0; i < tabSels.childNodes.length; i++) {
     child = tabSels.childNodes[i];
-    if (child.className === "tabSelector") {
+
+    // Check if the current child is a tab selector.
+    if (child.className != undefined &&
+        child.className.indexOf("tabSelector") != -1) {
       // Color the current tab selector.
-      if (child.id == tabSelectorName) {
-	      child.style.backgroundColor = "red";
+      if (child == tabSelector) {
+        // In case it is already present, remove the selected class first.
+        child.className = child.className.replace(" selectedTab", "");
+        child.className = child.className + " selectedTab";
       } else {
-	      child.style.backgroundColor = "white";
+        child.className = child.className.replace(" selectedTab", "");
       }
     }
   }
+  setupProfileView();
 };
+
+// Setup the profile view for the active tab. If the Browse tab is active
+// and no email is given, display the wall for the cached email in browsedUserActive.
+var setupProfileView = function(email){
+  // Check which tab is active.
+  var mainDiv, result;
+  if (document.getElementById("homeTab").style.display == "block") {
+    mainDiv = document.getElementById("homeTabBody");
+    result = serverstub.getUserDataByToken(localStorage.token);
+  } else if(document.getElementById("browseTab").style.display == "block") {
+    mainDiv = document.getElementById("browseTabBody");
+    if (email != undefined) browsedUserActive = email;
+    if (browsedUserActive != undefined && browsedUserActive != "")
+      result = serverstub.getUserDataByEmail(localStorage.token, browsedUserActive);
+  }
+  else return;  // No tab with a main view active.
+
+  // Clear out old data.
+  document.getElementById("homeTabBody").innerHTML = "";
+  document.getElementById("browseTabBody").innerHTML = "";
+
+  if (result == undefined ) return;
+  if (result["success"] == false) {
+    // Set status message eg "No such user.".
+    document.getElementById("searchUserStatus").innerText = result["message"];
+    return;
+  }
+
+  // Display user data in the side view.
+  mainDiv.innerHTML = document.getElementById("homebrowseview").innerHTML;
+  var userData = result["data"];
+  document.getElementById("userInfoFirst").innerText = userData["firstname"];
+  document.getElementById("userInfoFamily").innerText = userData["familyname"];
+  document.getElementById("userInfoGender").innerText = userData["gender"];
+  document.getElementById("userInfoCity").innerText = userData["city"];
+  document.getElementById("userInfoCountry").innerText = userData["country"];
+  document.getElementById("userInfoEmail").innerText = userData["email"];
+
+  reloadWall();
+}
+
+var reloadWall = function(){
+  var email = document.getElementById("userInfoEmail").innerText;
+  var result = serverstub.getUserMessagesByEmail(localStorage.token, email);
+  if (result["success"] == false) {
+    alert("Error: " + result["message"]);
+    return;
+  }
+  var posts = result["data"];
+  var wall = document.getElementById("wall");
+  // Clear old posts.
+  wall.innerHTML = "";
+
+  // Iterate through the list and create visual post elements for every entry.
+  for (var i = 0; i < posts.length; i++) {
+    var post = document.createElement("div");
+    post.className = "wallPost";
+    post.innerHTML = "<b>" + posts[i]["writer"] + "</b><br>" + posts[i]["content"];
+    wall.appendChild(post);
+  }
+}
+
+// Posts a message to the currently displayed user.
+var postMessage = function(){
+  var form = document.forms["submitpost"];
+  if (!validateNonEmpty(form)) {
+    return;
+  }
+
+  var email = document.getElementById("userInfoEmail").innerText;
+  var message = form["postinput"];
+
+  var result = serverstub.postMessage(localStorage.token, message.value, email);
+  if (result["success"] == false) {
+    alert("Error: " + result["message"]);
+    return;
+  }
+  reloadWall(email);
+  message.value = "";  // Empty post textarea after successful post.
+}
+
+var searchUser = function(){
+  var form = document.forms["searchuser"];
+  if (!validateNonEmpty(form)) {
+    return;
+  }
+
+  email = form["searchemail"].value;
+  setupProfileView(email);
+}
 
 // Sign out on server.
 var logout = function(){
@@ -173,7 +291,8 @@ var logout = function(){
     alert("Error logging out: " + result["message"]);
   } else {
     localStorage.token = "";  // Remove token from local storage.
-    setCurrentView();
+    browsedUserActive = "";  // Browse tab should not be cached for the next user that logs in.
+    displayView();  // Switch back to Welcome view.
   }
 };
 
@@ -190,11 +309,11 @@ var changePwd = function(){
     document.getElementById("changePwdStatus").innerText = result["message"];
     if (result["success"] == false) {
       addError(form["oldpwd"]);
+    } else {
+      // Clear all fields after successful password change.
+      form["oldpwd"].value = "";
+      form["newpassword1"].value = "";
+      form["newpassword2"].value = "";
     }
-
-    // Clear all fields.
-    form["oldpwd"].value = "";
-    form["newpassword1"].value = "";
-    form["newpassword2"].value = "";
   }
 };
